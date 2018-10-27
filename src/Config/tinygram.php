7b52f12e86a1bb0cc6744bb41f6a176f8ef9ed81
tristanward <tristan.ward@dewsign.co.uk>
2018-10-27 12:38:49 +0100

    include config file

---
+++ src/Config/tinygram.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    // Instagram access token
+    'token' => env('INSTAGRAM_ACCESS_TOKEN'),
+    'recentMediaBaseUrl' => 'https://api.instagram.com/v1/users/self/media/recent/?access_token=',
+];
---
+++ src/Providers/TinygramServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TinygramServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishConfigs();
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            $this->getConfigsPath(),
+            'tinygram'
+        );
+    }
+
+    /**
+     * Publish configuration file.
+     *
+     * @return void
+     */
+    private function publishConfigs()
+    {
+        $this->publishes([
+            $this->getConfigsPath() => config_path('tinygram.php'),
+        ], 'config');
+    }
+
+    /**
+     * Get local package configuration path.
+     *
+     * @return string
+     */
+    private function getConfigsPath()
+    {
+        return __DIR__.'/../Config/tinygram.php';
+    }
+}
